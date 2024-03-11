27059334c8b369c0f761357e21b6035dc54ce982
rotek123 <145429033+rotek123@users.noreply.github.com>
2024-03-11 08:43:31 +0100

    Update streamlit_app.py
    Fixed syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ st.write('The name of your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on')
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
